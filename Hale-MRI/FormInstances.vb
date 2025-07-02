0ed8afeb16b37fbb3d53f538fec362943a06436d
michael B <mbrodskiis@gmail.com>
2025-07-02 14:46:53 -0600

    Form Nav

---
+++ Hale-MRI/FormInstances.vb
@@ -0,0 +1,23 @@
+﻿' This module manages creating, showing and disposing of application forms.
+Module FormInstances
+    Public Sub ShowForm(Of F As {Form, New})(ByRef frm As F)
+        frm = Application.OpenForms.OfType(Of F)().FirstOrDefault()
+        If frm Is Nothing OrElse Not frm.IsHandleCreated Then
+            ' If no instance of the form is open, create and show a new instance
+            frm = New F()
+            frm.Show()
+        Else
+            ' If an instance is already open, bring it to the front
+            frm.WindowState = FormWindowState.Normal
+            frm.BringToFront()
+        End If
+    End Sub
+    Public Sub CloseForm(Of F As {Form, New})(ByRef frm As F)
+        If frm IsNot Nothing AndAlso frm.IsHandleCreated Then
+            ' Close the form if it is open
+            frm.Close()
+            frm.Dispose()
+            frm = Nothing
+        End If
+    End Sub
+End Module
