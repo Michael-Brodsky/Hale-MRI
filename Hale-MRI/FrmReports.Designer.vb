04dc0353e5010e3136d3795b212af72445fdb72c
michael B <mbrodskiis@gmail.com>
2026-02-04 12:01:48 -0700

    FrmReports260104_2

--- Hale-MRI/FrmReports.Designer.vb
+++ Hale-MRI/FrmReports.Designer.vb
@@ -24,12 +24,12 @@ Partial Class FrmReports
     Private Sub InitializeComponent()
         components = New ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmReports))
-        Dim ChartArea3 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New DataVisualization.Charting.ChartArea()
-        Dim Legend3 As System.Windows.Forms.DataVisualization.Charting.Legend = New DataVisualization.Charting.Legend()
-        Dim Series3 As System.Windows.Forms.DataVisualization.Charting.Series = New DataVisualization.Charting.Series()
-        Dim ChartArea4 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New DataVisualization.Charting.ChartArea()
-        Dim Legend4 As System.Windows.Forms.DataVisualization.Charting.Legend = New DataVisualization.Charting.Legend()
-        Dim Series4 As System.Windows.Forms.DataVisualization.Charting.Series = New DataVisualization.Charting.Series()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New DataVisualization.Charting.Series()
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New DataVisualization.Charting.ChartArea()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New DataVisualization.Charting.Legend()
+        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New DataVisualization.Charting.Series()
         ReportsBindingSource = New BindingSource(components)
         FormMenuStrip = New MenuStrip()
         FileToolStripMenuItem = New ToolStripMenuItem()
@@ -55,6 +55,11 @@ Partial Class FrmReports
         DeleteToolStripMenuItem = New ToolStripMenuItem()
         ToolStripSeparator9 = New ToolStripSeparator()
         SelectAllToolStripMenuItem = New ToolStripMenuItem()
+        JobsToolStripMenuItem = New ToolStripMenuItem()
+        JobsRecentToolStripMenuItem = New ToolStripMenuItem()
+        JobsOpenToolStripMenuItem = New ToolStripMenuItem()
+        ToolStripSeparator13 = New ToolStripSeparator()
+        JobsCloseToolStripMenuItem = New ToolStripMenuItem()
         ReportsToolStripMenuItem = New ToolStripMenuItem()
         ReportsToolStripSeparator1 = New ToolStripSeparator()
         ReportsEditToolStripMenuItem = New ToolStripMenuItem()
@@ -145,11 +150,7 @@ Partial Class FrmReports
         PageSetupDialog = New PageSetupDialog()
         ChartBladeHeight = New DataVisualization.Charting.Chart()
         ChartAngularPosition = New DataVisualization.Charting.Chart()
-        JobsToolStripMenuItem = New ToolStripMenuItem()
-        JobsRecentToolStripMenuItem = New ToolStripMenuItem()
-        JobsOpenToolStripMenuItem = New ToolStripMenuItem()
-        ToolStripSeparator13 = New ToolStripSeparator()
-        JobsCloseToolStripMenuItem = New ToolStripMenuItem()
+        FileRecentToolStripMenuItem = New ToolStripMenuItem()
         CType(ReportsBindingSource, ComponentModel.ISupportInitialize).BeginInit()
         FormMenuStrip.SuspendLayout()
         ReportContextMenuStrip.SuspendLayout()
@@ -181,7 +182,7 @@ Partial Class FrmReports
         ' 
         ' FileToolStripMenuItem
         ' 
-        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {FileNewToolStripMenuItem, ToolStripSeparator11, OpenToolStripMenuItem, ToolStripSeparator1, CloseToolStripMenuItem, ToolStripSeparator3, SaveToolStripMenuItem, SaveAsToolStripMenuItem, ToolStripSeparator4, FilePrintToolStripMenuItem, ToolStripSeparator2, ExitToolStripMenuItem})
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {FileNewToolStripMenuItem, ToolStripSeparator11, OpenToolStripMenuItem, FileRecentToolStripMenuItem, ToolStripSeparator1, CloseToolStripMenuItem, ToolStripSeparator3, SaveToolStripMenuItem, SaveAsToolStripMenuItem, ToolStripSeparator4, FilePrintToolStripMenuItem, ToolStripSeparator2, ExitToolStripMenuItem})
         FileToolStripMenuItem.Name = "FileToolStripMenuItem"
         FileToolStripMenuItem.Size = New Size(37, 20)
         FileToolStripMenuItem.Text = "File"
@@ -340,6 +341,36 @@ Partial Class FrmReports
         SelectAllToolStripMenuItem.Size = New Size(164, 22)
         SelectAllToolStripMenuItem.Text = "Select All"
         ' 
+        ' JobsToolStripMenuItem
+        ' 
+        JobsToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {JobsOpenToolStripMenuItem, JobsRecentToolStripMenuItem, ToolStripSeparator13, JobsCloseToolStripMenuItem})
+        JobsToolStripMenuItem.Name = "JobsToolStripMenuItem"
+        JobsToolStripMenuItem.Size = New Size(42, 20)
+        JobsToolStripMenuItem.Text = "Jobs"
+        ' 
+        ' JobsRecentToolStripMenuItem
+        ' 
+        JobsRecentToolStripMenuItem.Name = "JobsRecentToolStripMenuItem"
+        JobsRecentToolStripMenuItem.Size = New Size(180, 22)
+        JobsRecentToolStripMenuItem.Text = "Recent"
+        ' 
+        ' JobsOpenToolStripMenuItem
+        ' 
+        JobsOpenToolStripMenuItem.Name = "JobsOpenToolStripMenuItem"
+        JobsOpenToolStripMenuItem.Size = New Size(180, 22)
+        JobsOpenToolStripMenuItem.Text = "Open"
+        ' 
+        ' ToolStripSeparator13
+        ' 
+        ToolStripSeparator13.Name = "ToolStripSeparator13"
+        ToolStripSeparator13.Size = New Size(177, 6)
+        ' 
+        ' JobsCloseToolStripMenuItem
+        ' 
+        JobsCloseToolStripMenuItem.Name = "JobsCloseToolStripMenuItem"
+        JobsCloseToolStripMenuItem.Size = New Size(180, 22)
+        JobsCloseToolStripMenuItem.Text = "Close"
+        ' 
         ' ReportsToolStripMenuItem
         ' 
         ReportsToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ReportsToolStripSeparator1, ReportsEditToolStripMenuItem, ReportsImportToolStripMenuItem, ReportsExportToolStripMenuItem})
@@ -350,25 +381,25 @@ Partial Class FrmReports
         ' ReportsToolStripSeparator1
         ' 
         ReportsToolStripSeparator1.Name = "ReportsToolStripSeparator1"
-        ReportsToolStripSeparator1.Size = New Size(177, 6)
+        ReportsToolStripSeparator1.Size = New Size(107, 6)
         ' 
         ' ReportsEditToolStripMenuItem
         ' 
         ReportsEditToolStripMenuItem.Name = "ReportsEditToolStripMenuItem"
-        ReportsEditToolStripMenuItem.Size = New Size(180, 22)
+        ReportsEditToolStripMenuItem.Size = New Size(110, 22)
         ReportsEditToolStripMenuItem.Text = "Edit"
         ' 
         ' ReportsImportToolStripMenuItem
         ' 
         ReportsImportToolStripMenuItem.Name = "ReportsImportToolStripMenuItem"
-        ReportsImportToolStripMenuItem.Size = New Size(180, 22)
+        ReportsImportToolStripMenuItem.Size = New Size(110, 22)
         ReportsImportToolStripMenuItem.Text = "Import"
         ' 
         ' ReportsExportToolStripMenuItem
         ' 
         ReportsExportToolStripMenuItem.Enabled = False
         ReportsExportToolStripMenuItem.Name = "ReportsExportToolStripMenuItem"
-        ReportsExportToolStripMenuItem.Size = New Size(180, 22)
+        ReportsExportToolStripMenuItem.Size = New Size(110, 22)
         ReportsExportToolStripMenuItem.Text = "Export"
         ' 
         ' ElementsToolStripMenuItem
@@ -1313,17 +1344,17 @@ Partial Class FrmReports
         ' 
         ' ChartBladeHeight
         ' 
-        ChartArea3.Name = "ChartArea1"
-        ChartBladeHeight.ChartAreas.Add(ChartArea3)
-        Legend3.Name = "Legend1"
-        ChartBladeHeight.Legends.Add(Legend3)
+        ChartArea1.Name = "ChartArea1"
+        ChartBladeHeight.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        ChartBladeHeight.Legends.Add(Legend1)
         ChartBladeHeight.Location = New Point(13, 388)
         ChartBladeHeight.Margin = New Padding(0, 1, 0, 0)
         ChartBladeHeight.Name = "ChartBladeHeight"
-        Series3.ChartArea = "ChartArea1"
-        Series3.Legend = "Legend1"
-        Series3.Name = "Series1"
-        ChartBladeHeight.Series.Add(Series3)
+        Series1.ChartArea = "ChartArea1"
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        ChartBladeHeight.Series.Add(Series1)
         ChartBladeHeight.Size = New Size(312, 137)
         ChartBladeHeight.TabIndex = 25
         ChartBladeHeight.Text = "Track"
@@ -1331,50 +1362,26 @@ Partial Class FrmReports
         ' 
         ' ChartAngularPosition
         ' 
-        ChartArea4.Name = "ChartArea1"
-        ChartAngularPosition.ChartAreas.Add(ChartArea4)
-        Legend4.Name = "Legend1"
-        ChartAngularPosition.Legends.Add(Legend4)
+        ChartArea2.Name = "ChartArea1"
+        ChartAngularPosition.ChartAreas.Add(ChartArea2)
+        Legend2.Name = "Legend1"
+        ChartAngularPosition.Legends.Add(Legend2)
         ChartAngularPosition.Location = New Point(340, 388)
         ChartAngularPosition.Name = "ChartAngularPosition"
-        Series4.ChartArea = "ChartArea1"
-        Series4.Legend = "Legend1"
-        Series4.Name = "Series1"
-        ChartAngularPosition.Series.Add(Series4)
+        Series2.ChartArea = "ChartArea1"
+        Series2.Legend = "Legend1"
+        Series2.Name = "Series1"
+        ChartAngularPosition.Series.Add(Series2)
         ChartAngularPosition.Size = New Size(306, 137)
         ChartAngularPosition.TabIndex = 26
         ChartAngularPosition.Text = "Track"
         ChartAngularPosition.Visible = False
         ' 
-        ' JobsToolStripMenuItem
+        ' FileRecentToolStripMenuItem
         ' 
-        JobsToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {JobsRecentToolStripMenuItem, JobsOpenToolStripMenuItem, ToolStripSeparator13, JobsCloseToolStripMenuItem})
-        JobsToolStripMenuItem.Name = "JobsToolStripMenuItem"
-        JobsToolStripMenuItem.Size = New Size(42, 20)
-        JobsToolStripMenuItem.Text = "Jobs"
-        ' 
-        ' JobsRecentToolStripMenuItem
-        ' 
-        JobsRecentToolStripMenuItem.Name = "JobsRecentToolStripMenuItem"
-        JobsRecentToolStripMenuItem.Size = New Size(180, 22)
-        JobsRecentToolStripMenuItem.Text = "Recent"
-        ' 
-        ' JobsOpenToolStripMenuItem
-        ' 
-        JobsOpenToolStripMenuItem.Name = "JobsOpenToolStripMenuItem"
-        JobsOpenToolStripMenuItem.Size = New Size(180, 22)
-        JobsOpenToolStripMenuItem.Text = "Open"
-        ' 
-        ' ToolStripSeparator13
-        ' 
-        ToolStripSeparator13.Name = "ToolStripSeparator13"
-        ToolStripSeparator13.Size = New Size(177, 6)
-        ' 
-        ' JobsCloseToolStripMenuItem
-        ' 
-        JobsCloseToolStripMenuItem.Name = "JobsCloseToolStripMenuItem"
-        JobsCloseToolStripMenuItem.Size = New Size(180, 22)
-        JobsCloseToolStripMenuItem.Text = "Close"
+        FileRecentToolStripMenuItem.Name = "FileRecentToolStripMenuItem"
+        FileRecentToolStripMenuItem.Size = New Size(186, 22)
+        FileRecentToolStripMenuItem.Text = "Recent"
         ' 
         ' FrmReports
         ' 
@@ -1528,4 +1535,5 @@ Partial Class FrmReports
     Friend WithEvents JobsOpenToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ToolStripSeparator13 As ToolStripSeparator
     Friend WithEvents JobsCloseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FileRecentToolStripMenuItem As ToolStripMenuItem
 End Class
---
+++ Hale-MRI/Reporting.vb
@@ -0,0 +1,85 @@
+﻿Imports System.Windows.Forms.DataVisualization.Charting
+Imports LibDatabase.Models
+
+Module Reporting
+    ' ReportDataDelegate type - a delegate method that can be
+    ' invoked when a client control needs to update its data.
+    Public Class ReportDataArgs
+        Public Property Args() As Object
+
+        Public Sub New()
+
+        End Sub
+
+        Public Sub New(ByVal ParamArray args())
+            Me.Args = args
+        End Sub
+    End Class
+
+    Public Delegate Sub ReportDataDelegate(ByRef sender As Object, e As ReportDataArgs)
+
+    Public Sub ChartBladeHeight_Data(ByRef sender As Object, e As ReportDataArgs)
+        Const kHeightOffset As Double = 0.2 ' Offset to add to data points for visual comparison?
+        Dim client As Chart = CType(sender, Chart)
+        Dim refBlade As Integer? = CType(e.Args(0), Integer?)
+        Dim refPoint As String = CType(e.Args(1), String)
+        Dim refRadius As Double = CType(e.Args(2), Double)
+        Dim data As JobDetail = CType(e.Args(3), JobDetail)
+
+        If refBlade IsNot Nothing AndAlso refPoint IsNot Nothing AndAlso refRadius > 0 Then
+            Dim seriesHeight As Series = ChartCreateSeries(client, "BladeHeight", "Blade", "Height")
+            Dim radiusMeasurements As List(Of RadiusMeasurement) = data?.RadiusMeasurements?.Where(Function(r) r.BladeId = refBlade).OrderBy(Function(r) CType(r.Radius, Double)).ToList()
+            Dim innerRm As RadiusMeasurement = radiusMeasurements?.FirstOrDefault() ' RadiusMeasurement at smallest radius
+            Dim outerRm As RadiusMeasurement = radiusMeasurements?.LastOrDefault()  ' RadiusMeasurement at largest radius
+            Dim refRm As RadiusMeasurement = radiusMeasurements?.FirstOrDefault(Function(r) Math.Round(CType(r.Radius, Double)) = refRadius)    ' RadiusMeasurement at reference radius
+            Dim refDepth As Double = TrackGetDepth(refRm, refPoint)                 ' Depth at reference radius and point
+            Dim refAngle As Double = TrackGetAngle(refRm, refPoint)                 ' Angle at reference radius and point
+            ' Plot each blade's data points
+            If innerRm Is Nothing Or outerRm Is Nothing Then
+                Return
+            End If
+            For i As Integer = 1 To data.Job.PropellerBlades
+                Dim b As Integer = i
+                Dim rm As RadiusMeasurement = data?.RadiusMeasurements?.FirstOrDefault(Function(r) r.BladeId = b)
+                If rm IsNot Nothing Then
+                    Dim bladeDepth As Double = TrackGetDepth(rm, refPoint)
+                    Dim bladeHeight As Double = Math.Abs(refDepth - bladeDepth) + kHeightOffset
+                    ChartAddPoint(client, seriesHeight, $"{b}", bladeHeight, (b = refBlade))
+                End If
+            Next
+        End If
+    End Sub
+
+    Public Sub ChartAngularPosition_Data(ByRef sender As Object, e As ReportDataArgs)
+        Const kHeightOffset As Double = 0.2 ' Offset to add to data points for visual comparison?
+        Dim client As Chart = CType(sender, Chart)
+        Dim refBlade As Integer? = CType(e.Args(0), Integer?)
+        Dim refPoint As String = CType(e.Args(1), String)
+        Dim refRadius As Double = CType(e.Args(2), Double)
+        Dim data As JobDetail = CType(e.Args(3), JobDetail)
+
+        If refBlade IsNot Nothing AndAlso refPoint IsNot Nothing AndAlso refRadius > 0 Then
+            Dim seriesPosition As Series = ChartCreateSeries(client, "AngularPosition", "Blade", "Position")
+            Dim radiusMeasurements As List(Of RadiusMeasurement) = data?.RadiusMeasurements?.Where(Function(r) r.BladeId = refBlade).OrderBy(Function(r) CType(r.Radius, Double)).ToList()
+            Dim innerRm As RadiusMeasurement = radiusMeasurements?.FirstOrDefault() ' RadiusMeasurement at smallest radius
+            Dim outerRm As RadiusMeasurement = radiusMeasurements?.LastOrDefault()  ' RadiusMeasurement at largest radius
+            Dim refRm As RadiusMeasurement = radiusMeasurements?.FirstOrDefault(Function(r) Math.Round(CType(r.Radius, Double)) = refRadius)    ' RadiusMeasurement at reference radius
+            Dim refDepth As Double = TrackGetDepth(refRm, refPoint)                 ' Depth at reference radius and point
+            Dim refAngle As Double = TrackGetAngle(refRm, refPoint)                 ' Angle at reference radius and point
+
+            ' Plot each blade's data points
+            If innerRm Is Nothing Or outerRm Is Nothing Then
+                Return
+            End If
+            For i As Integer = 1 To data.Job.PropellerBlades
+                Dim b As Integer = i
+                Dim rm As RadiusMeasurement = data?.RadiusMeasurements?.FirstOrDefault(Function(r) r.BladeId = b)
+                If rm IsNot Nothing Then
+                    Dim bladeAngle As Double = TrackGetAngle(rm, refPoint)
+                    Dim bladePosition As Double = Math.Abs(refAngle - bladeAngle) - ((360 / data.Job.PropellerBlades) * Math.Abs(refBlade.Value - rm.BladeId.Value)) + kHeightOffset
+                    ChartAddPoint(client, seriesPosition, $"{b}", bladePosition, (b = refBlade))
+                End If
+            Next
+        End If
+    End Sub
+End Module
